1b64105c25b2580641799691e4cc35036f66b0f1
Brian McAllister <brian@brianmcallister.com>
2020-03-12 13:34:09 -0400

    Add stylelint.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  plugins: [
+    '@typescript-eslint',
+    'eslint-comments',
+    'jest',
+    'promise',
+    'react-hooks',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  extends: [
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:jest/recommended',
+    'plugin:promise/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'import/no-default-export': 2,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/mouse-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'react/no-did-update-set-state': 'off',
+  },
+}
