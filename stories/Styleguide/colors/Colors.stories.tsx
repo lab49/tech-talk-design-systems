271dd74982d94a144b07338502ed534afa408963
Brian McAllister <brian@brianmcallister.com>
2020-03-13 17:32:40 -0400

    Add colors to the colors story.

--- stories/Styleguide/colors/Colors.stories.tsx
+++ stories/Styleguide/colors/Colors.stories.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 
-// import { Button } from './Button';
+import './_colors.scss';
+
+const baseClass = 'styleguide-colors';
 
 // eslint-disable-next-line import/no-default-export
 export default {
@@ -9,10 +11,27 @@ export default {
   decorators: [withKnobs],
 };
 
+const colors = [
+  { variable: 'sun', name: 'Sun', code: '#aaa' },
+  { variable: 'mercury', name: 'Mercury', code: '#2684db' },
+  { variable: 'venus', name: 'Venus', code: '#fff' },
+  { variable: 'earth', name: 'Earth', code: '#f0f0f0' },
+  { variable: 'mars', name: 'Mars', code: '#e65036' },
+  { variable: 'jupiter', name: 'Jupiter', code: '#c9c9c9' },
+];
+
 export const Default = () => {
   return (
-    <div>
-      colors
+    <div className={baseClass}>
+      {colors.map(({ variable, name, code }) => (
+        <div className={`${baseClass}__color ${baseClass}__color--${variable}`}>
+          <p className={`${baseClass}__color-code`}>{code}</p>
+
+          <p className={`${baseClass}__color-name`}>{name}</p>
+
+          <p className={`${baseClass}__color-variable`}>map-get($colors, {variable})</p>
+        </div>
+      ))}
     </div>
   );
 };
